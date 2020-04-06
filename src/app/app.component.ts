83f2c71311824ed93556070f236085f0d8099d4c
ErfanRaoofi <erfan.raoofi@gmail.com>
2020-04-06 12:59:19 +0430

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  tempNum = '0';
+  sumNum = 'sum';
+
+  public getNumber(v: string) {
+    this.tempNum === '0' ? this.tempNum = v : this.tempNum += v;
+  }
+
+  clear(v: string) {
+    this.tempNum = '0';
+    this.sumNum = 'sum';
+  }
+
+  getOperation(v: string) {
+       this.tempNum += v;
+  }
+
+  getOperationEval() {
+    this.sumNum = eval(this.tempNum);
+  }
+
+
 }
